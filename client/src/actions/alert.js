1d0fcd6a0d0331854199c88f3a843368e847a528
Eric Jiang <ericjiang1988@gmail.com>
2020-12-03 21:28:16 -0800

    Prepared for deployment

---
+++ client/src/actions/alert.js
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+export const setAlert = (msg, alertType, timeout = 5000) => (dispath) => {
+    const id = uuidv4();
+    dispath({
+        type: SET_ALERT,
+        payload: { msg, alertType, id },
+    });
+
+    setTimeout(
+        () =>
+            dispath({
+                type: REMOVE_ALERT,
+                payload: id,
+            }),
+        timeout
+    );
+};
